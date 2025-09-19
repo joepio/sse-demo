0d3ecad3adc0b2c66c2d18b3f47912d3e5c046c9
Joep Meindertsma <joep@ontola.io>
2025-09-19 10:36:35 +0200

    React 19 compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,13 @@ const serverUrl = "http://localhost:8000";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/events": {
